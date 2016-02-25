08c257ddc597d68180fdf429cd3f52cade2b91ab
Joschka Lingemann <joschka.lingemann@cern.ch>
2016-02-25 10:27:27 +0100

    Fix LD_LIBRARY_PATH and add podio.

--- init.sh
+++ init.sh
@@ -5,10 +5,11 @@ source /afs/cern.ch/exp/fcc/sw/0.6/LCG_80/ROOT/6.04.06/x86_64-slc6-gcc49-opt/bin
 
 export PYTHIA8_DIR=/afs/cern.ch/exp/fcc/sw/0.6/LCG_80/MCGenerators/pythia8/212/x86_64-slc6-gcc49-opt
 export HEPMC_PREFIX=/afs/cern.ch/exp/fcc/sw/0.6/LCG_80/HepMC/2.06.09/x86_64-slc6-gcc49-opt
-export FCC_EDM_DIR=/afs/cern.ch/exp/fcc/sw/0.5/fcc-edm/0.2/x86_64-slc6-gcc49-opt
+export FCC_EDM_DIR=/afs/cern.ch/exp/fcc/sw/0.6/fcc-edm/0.2/x86_64-slc6-gcc49-opt
+export PODIO_DIR=/afs/cern.ch/exp/fcc/sw/0.6/podio/0.2/x86_64-slc6-gcc49-opt
 
 export PYTHIA8DATA=${PYTHIA8_DIR}/share/Pythia8/xmldoc
 export PYTHIAFCC=$PWD/install
-export LD_LIBRARY_PATH=$PYTHIAFCC/lib:$HEPMC_PREFIX/lib:$FASTJET_ROOT_DIR/lib:$PYTHIA8_DIR/lib:FCC_EDM_DIR/lib:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=$PYTHIAFCC/lib:$HEPMC_PREFIX/lib:$FASTJET_ROOT_DIR/lib:$PYTHIA8_DIR/lib:$FCC_EDM_DIR/lib:$PODIO_DIR/lib:$LD_LIBRARY_PATH
 export PATH=$PYTHIAFCC/bin:$FASTJET_ROOT_DIR/bin:$PATH
 
